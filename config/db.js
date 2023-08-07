9414639fba687a476e611929c13122e8767471e2
Htripathi07 <tripathihariom34@gmail.com>
2023-08-07 23:48:41 +0530

    mock

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 
-const connection = mongoose.connect(process.env.MONGO_URL);
+const connection = mongoose.connect("mongodb+srv://tripathihariom34:fdcvKTacjdOxURap@cluster0.yjmosc6.mongodb.net/olx_classifieds");
 module.exports = { connection };
