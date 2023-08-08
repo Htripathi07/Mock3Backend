ad2e471c4c857973655fc5ab73324c6ec7559216
Htripathi07 <tripathihariom34@gmail.com>
2023-08-08 13:55:35 +0530

    mock baqnend

--- config/db.js
+++ config/db.js
@@ -2,5 +2,5 @@ const mongoose = require("mongoose");
 require("dotenv").config()
 
 
-const connection = mongoose.connect("mongodb+srv://tripathihariom34:fdcvKTacjdOxURap@cluster0.yjmosc6.mongodb.net/olx_classifieds");
+const connection = mongoose.connect(process.env.MONGO_URL);
 module.exports = { connection };
--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.get("/",(req,res)=>{
 app.use("/classifieds", classifiedsRoutes);
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 app.listen(PORT, async () => {
     console.log(`Server is running on port ${PORT}`);
   try {
